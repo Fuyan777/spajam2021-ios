bc43d47649363e7675aef29a6f0df9cd4bc39829
Fuya Yamada <fuyamiwa713@gmail.com>
2021-10-22 10:22:15 +0900

    add: Entity

---
+++ spajam-ios/Model/EntityModel.swift
@@ -0,0 +1,14 @@
+//
+//  EntityModel.swift
+//  spajam-ios
+//
+//  Created by yamada fuuya on 2021/10/22.
+//
+
+import Foundation
+/// sample data
+
+struct EntityModel: Codable {
+    var title: String
+    var url: String
+}
